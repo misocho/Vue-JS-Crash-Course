4cbd81954251bdace5e76beaf96c5c5b7cb6fb5c
Brian Moranga <brian.moranga@infocap.co.ke>
2020-05-07 07:12:27 +0300

    Vue todolist add

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,13 +1,18 @@
 <template>
   <header class="header">
     <h1>TodoList</h1>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view />
   </header>
 </template>
 
 <script>
 export default {
   name: 'Header',
-}
+};
 </script>
 
 <style scoped>
@@ -21,5 +26,19 @@ export default {
 .header a {
   color: #fff;
   padding-right: 5px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #fff;
+  text-decoration: none;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
